25618ceedb3083b9281fbcb3804d8771eb5f396b
Dave Fisher <dave.j.fish@gmail.com>
2022-10-03 18:54:29 -0700

    win conditions working

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,22 +3,16 @@ import { GameContext } from '../../context/GameContext';
 import './Board.css';
 
 export default function Board() {
-  const { gameBoard, setGameBoard } = useContext(GameContext);
-
-  const handleClick = (box) => {
-    const newBoard = gameBoard.map((square) => {
-      if (square.position === box.position) {
-        return { position: box.position, content: 'ye' };
-      }
-      return square;
-    });
-    setGameBoard(newBoard);
-  };
+  const { gameBoard, handleClick } = useContext(GameContext);
     
   return (
     <div className='game-board'>
       {gameBoard.map(box => (
-        <div className='box' onClick={() => handleClick(box)} key={box.position}>{box.content}</div>
+        <div className='box' 
+          onClick={() => handleClick(box)} 
+          key={box.position}>
+          {box.content}
+        </div>
       ))}
     </div>
   );
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -6,9 +6,57 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [gameBoard, setGameBoard] = useState(newGame);
+  const [player, setPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const setTurn = () => {
+    player === 'X' ? setPlayer('O') : setPlayer('X');
+  };
+
+  const checkWinner = (gameBoard) => {
+    const checkRows = (a, b, c) => {
+      if (gameBoard[a].content === '' || gameBoard[b].content === '' || gameBoard[c].content === '')
+        return false;
+      return (
+        gameBoard[a].content === gameBoard[b].content && 
+        gameBoard[b].content === gameBoard[c].content);
+    };
+
+    return (
+      checkRows(0, 1, 2) || 
+      checkRows(3, 4, 5) || 
+      checkRows(6, 7, 8) || 
+      checkRows(0, 3, 6) || 
+      checkRows(1, 4, 7) || 
+      checkRows(2, 5, 8) || 
+      checkRows(0, 4, 8) || 
+      checkRows(2, 4, 6)
+    );
+  };
+
+  const handleClick = (box) => {
+    if (box.content !== '') return;
+
+    const newBoard = gameBoard.map((square) => {
+      if (square.position === box.position) {
+        return { 
+          position: box.position, 
+          content: (player === 'X' ? 'X' : 'O') };
+      }
+      return square;
+    });
+
+    setGameBoard(newBoard);
+
+    if (checkWinner(newBoard)) {
+      setWinner(player);
+    }
+
+    setTurn();
+  };
 
   return (
-    <GameContext.Provider value={{ gameBoard, setGameBoard }}>
+    <GameContext.Provider value={{ gameBoard, handleClick }}>
       {children}
     </GameContext.Provider>
   );
